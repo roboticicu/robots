92b13fcc0c18bfe1b62d515df7a04f1f9013ee2c
David Kershner <David.Kershner@unisys.com>
2020-03-15 17:26:54 -0400

    Move variables to top of functions.. remove else case

--- OLEDespNTP/OLEDespNTP.ino
+++ OLEDespNTP/OLEDespNTP.ino
@@ -141,95 +141,104 @@ void setup() {
 }
 
 void loop() {
+	int pm=0;
+	int cb;
+	unsigned long highWord;
+	unsigned long lowWord;
+	unsigned long secsSince1900;
+	unsigned long epoch;
+	// Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+	//
+	const unsigned long seventyYears = 2208988800UL;
+	// subtract seventy years:
+
 	//get a random server from the pool
 	WiFi.hostByName(ntpServerName, timeServerIP);
 	sendNTPpacket(timeServerIP);
 	// send an NTP packet to a time server
 	// wait to see if a reply is available
 	delay(1000);
-	int cb = udp.parsePacket();
+	cb = udp.parsePacket();
 	if (!cb) {
 		Serial.println("no packet yet");
+		display.display();
+		// wait ten seconds before asking for the time again
+		delay(Interval);
+		return;
+	}
+	Serial.print("packet received, length=");
+	Serial.println(cb);
+	// We've received a packet, read the data from it
+	udp.read(packetBuffer, NTP_PACKET_SIZE);
+	// read the packet into the buffer
+	//the timestamp starts at byte 40 of the received packet and is four bytes,
+	// or two words, long. First, esxtract the two words:
+	highWord = word(packetBuffer[40], packetBuffer[41]);
+	lowWord = word(packetBuffer[42], packetBuffer[43]);
+	// combine the four bytes (two words) into a long integer
+	// this is NTP time (seconds since Jan 1 1900):
+	secsSince1900 = highWord << 16 | lowWord;
+	Serial.print("\n\nSeconds since Jan 1 1900 = " );
+	Serial.println(secsSince1900);
+	// now convert NTP time into everyday time:
+	Serial.print("Unix time = ");
+
+	/*
+	if(digitalRead(pin)){
+		int zone = 1;
 	} else {
-		Serial.print("packet received, length=");
-		Serial.println(cb);
-		// We've received a packet, read the data from it
-		udp.read(packetBuffer, NTP_PACKET_SIZE);
-		// read the packet into the buffer
-		//the timestamp starts at byte 40 of the received packet and is four bytes,
-		// or two words, long. First, esxtract the two words:
-		unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-		unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-		// combine the four bytes (two words) into a long integer
-		// this is NTP time (seconds since Jan 1 1900):
-		unsigned long secsSince1900 = highWord << 16 | lowWord;
-		Serial.print("\n\nSeconds since Jan 1 1900 = " );
-		Serial.println(secsSince1900);
-		// now convert NTP time into everyday time:
-		Serial.print("Unix time = ");
-		// Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-		//
-		const unsigned long seventyYears = 2208988800UL;
-		// subtract seventy years:
+		int zone = 2;
+	}
+	*/
 
+	epoch = secsSince1900 - seventyYears + zones[zone].offset;
+	// print Unix time:
+	Serial.println(epoch);
+	// print the hour, minute and second:
+	Serial.print("The UTC time is ");
+	// UTC is the time at Greenwich Meridian (GMT)
 
-		/*
-		if(digitalRead(pin)){
-			int zone = 1;
-		} else {
-			int zone = 2;
-		}
-		*/
+	DisplayTime();
 
-		unsigned long epoch = secsSince1900 - seventyYears + zones[zone].offset;
-		// print Unix time:
-		Serial.println(epoch);
-		// print the hour, minute and second:
-		Serial.print("The UTC time is ");
-		// UTC is the time at Greenwich Meridian (GMT)
-
-		DisplayTime();
- 
-		Serial.print((epoch % 86400L) / 3600);
-		if(((epoch % 86400L) / 3600)<10) {
-			display.print('0');
-		}
-		if(((epoch % 86400L) / 3600)>12) {
-			display.print('0');
-			display.print(((epoch % 86400L) / 3600)-12);
-			int pm=1;
-		} else if((((epoch % 86400L) / 3600)-12)<10) {
-			display.print(".");
-			display.print(((epoch % 86400L) / 3600)-12);  
-		} else {
-			display.print((epoch % 86400L) / 3600);
-			int pm=0;
-		}
-		// print the hour (86400 equals secs per day)
-		Serial.print(':');
-		display.print(':');
-		if ( ((epoch % 3600) / 60) < 10 ) {
-			// In the first 10 minutes of each hour, we'll want a leading '0'
-			Serial.print('0');
-			display.print('0');
-		}
-		Serial.print((epoch % 3600) / 60);
-		display.print((epoch % 3600) / 60);
-		// print the minute (3600 equals secs per minute)
-		Serial.print(':');
-		display.print(':');
-		if ( (epoch % 60) < 10 ) {
-			// In the first 10 seconds of each minute, we'll want a leading '0'
-			Serial.print('0');
-			display.print('0');
-		}
-		Serial.println(epoch % 60);
-		display.print(epoch % 60);
-		if (pm) {
-			display.println("p");
-		}
-		// print the second
- 	}
+	Serial.print((epoch % 86400L) / 3600);
+	if(((epoch % 86400L) / 3600)<10) {
+		display.print('0');
+	}
+	if(((epoch % 86400L) / 3600)>12) {
+		display.print('0');
+		display.print(((epoch % 86400L) / 3600)-12);
+		pm=1;
+	} else if((((epoch % 86400L) / 3600)-12)<10) {
+		display.print(".");
+		display.print(((epoch % 86400L) / 3600)-12);  
+	} else {
+		display.print((epoch % 86400L) / 3600);
+		pm=0;
+	}
+	// print the hour (86400 equals secs per day)
+	Serial.print(':');
+	display.print(':');
+	if ( ((epoch % 3600) / 60) < 10 ) {
+		// In the first 10 minutes of each hour, we'll want a leading '0'
+		Serial.print('0');
+		display.print('0');
+	}
+	Serial.print((epoch % 3600) / 60);
+	display.print((epoch % 3600) / 60);
+	// print the minute (3600 equals secs per minute)
+	Serial.print(':');
+	display.print(':');
+	if ( (epoch % 60) < 10 ) {
+		// In the first 10 seconds of each minute, we'll want a leading '0'
+		Serial.print('0');
+		display.print('0');
+	}
+	Serial.println(epoch % 60);
+	display.print(epoch % 60);
+	if (pm) {
+		display.println("p");
+	}
+	// print the second
 	display.display();
 	// wait ten seconds before asking for the time again
 	delay(Interval);
